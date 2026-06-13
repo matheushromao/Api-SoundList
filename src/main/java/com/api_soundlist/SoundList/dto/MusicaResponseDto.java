6ebdfc890a8a7e0fbb3df57d2b252ae01a8ccaa9
Matheus Romão <matheus_henrique_9@hotmail.com>
2026-06-12 22:17:06 -0300

    feat: adiciona restante do dto, service, exception, controller

---
+++ src/main/java/com/api_soundlist/SoundList/dto/MusicaResponseDto.java
@@ -0,0 +1,9 @@
+package com.api_soundlist.SoundList.dto;
+
+public record MusicaResponseDto(Long id,
+                                String title,
+                                String artist,
+                                String genre,
+                                Integer duration,
+                                Playlist playlist) {
+}
